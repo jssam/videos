fc1e94c0ff97f945c629f5c290c1106c0966fbe5
Sanyam jain <sanyamjain@Sanyams-Air.bbrouter>
2024-03-24 19:44:47 +0530

    added sign in sam

--- async_await.js
+++ async_await.js
@@ -3,5 +3,4 @@ console.log('Bacha comming');
 let res = await fetch('https://bachaSlip');
 let data = await fetch('https://papaSlip',res);
 console.log('Bacha Kicked:', data);
-console.log("sanyam")
 }
